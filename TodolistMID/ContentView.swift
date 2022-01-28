fe54e60570261c9e090d82fd82bb6939639394cc
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2022-01-28 18:12:31 +0300

    Initial commit.

---
+++ TodolistMID/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TodolistMID
+//
+//  Created by Maksim Ivanov on 28.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello00000!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TodolistMID/TodolistMIDApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistMIDApp.swift
+//  TodolistMID
+//
+//  Created by Maksim Ivanov on 28.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TodolistMIDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
