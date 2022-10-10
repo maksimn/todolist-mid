c5ad4fc03ff540fc40b7ffdbb78d2c0643612027
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2022-10-10 05:09:03 +0300

    [TDL-10]: TodoList Counter & VisibilitySwitch.

--- TodolistMID/Todolist/TodoListView.swift
+++ TodolistMID/Todolist/TodoListView.swift
@@ -16,34 +16,58 @@ struct TodoListView: View {
         WithViewStore(self.store) { viewStore in
             NavigationView {
                 VStack {
-                    List {
-                        ForEach(viewStore.state.items) { item in
-                            NavigationLink(destination: navigateToEditor(item)) {
-                                TodoItemCell(item: item)
-                                    .swipeActions(edge: .leading, allowsFullSwipe: false) {
-                                        Button(action: {
-                                            viewStore.send(.toggleItemCompletion(item))
-                                        }, label: {
-                                            Image("finished-todo-inverse")
-                                        })
-                                        .tint(.green)
-                                    }
-                            }
-                        }
-                        .onDelete { indexSet in
-                            for index in indexSet {
-                                let item = viewStore.state.items[index]
-
-                                viewStore.send(.deleteItem(item))
-                            }
-                        }
-
-                        NewTodoItemCell(
-                            text: "",
-                            onTextEnter: { text in
-                                viewStore.send(.createItem(TodoItem(text: text)))
+                    HStack {
+                        Text("Выполнено — \(viewStore.state.completedItemCount)")
+                            .foregroundColor(Color.gray)
+                            .padding(.init(top: 12, leading: 24, bottom: 0, trailing: 10))
+                        Spacer()
+                        Button(
+                            action: {
+                                viewStore.send(.switchCompletedItemsVisibility)
+                            },
+                            label: {
+                                Text(viewStore.state.areCompleteItemsVisible ? "Скрыть" : "Показать")
+                                    .fontWeight(.medium)
+                                    .foregroundColor(viewStore.state.completedItemCount == 0 ? Color.gray : Color.blue)
                             }
                         )
+                        .disabled(viewStore.state.completedItemCount == 0)
+                        .padding(.init(top: 12, leading: 10, bottom: 0, trailing: 26))
+                    }
+                    VStack {
+                        List {
+                            ForEach(
+                                viewStore.state.areCompleteItemsVisible ?
+                                viewStore.state.items :
+                                viewStore.state.items.filter { !$0.isCompleted }
+                            ) { item in
+                                NavigationLink(destination: navigateToEditor(item)) {
+                                    TodoItemCell(item: item)
+                                        .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                                            Button(action: {
+                                                viewStore.send(.toggleItemCompletion(item))
+                                            }, label: {
+                                                Image("finished-todo-inverse")
+                                            })
+                                            .tint(.green)
+                                        }
+                                }
+                            }
+                            .onDelete { indexSet in
+                                for index in indexSet {
+                                    let item = viewStore.state.items[index]
+
+                                    viewStore.send(.deleteItem(item))
+                                }
+                            }
+
+                            NewTodoItemCell(
+                                text: "",
+                                onTextEnter: { text in
+                                    viewStore.send(.createItem(TodoItem(text: text)))
+                                }
+                            )
+                        }
                     }
 
                     Spacer()
@@ -70,77 +94,5 @@ struct TodoListView: View {
                 initialItem: initialItem
             )
         )
-    }
-}
-
-struct TodoItemCell: View {
-
-    @State
-    var item: TodoItem
-
-    var body: some View {
-        HStack {
-            if item.isCompleted {
-                Image("finished-todo")
-            } else if item.priority == .high {
-                Image("high-priority-circle")
-            } else {
-                Image("not-finished-todo")
-            }
-
-            if item.priority == .low {
-                Image("low-priority")
-                    .padding(.init(top: 0, leading: 5, bottom: 0, trailing: 0))
-            } else if item.priority == .high {
-                Image("high-priority")
-                    .padding(.init(top: 0, leading: 6, bottom: 0, trailing: 0))
-            }
-
-            VStack {
-                HStack {
-                    Text(item.text)
-                        .strikethrough(item.isCompleted, color: Theme.data.lightTextColor)
-                        .foregroundColor(item.isCompleted ? Theme.data.lightTextColor : Color.black)
-                        .lineLimit(3)
-                        .padding(.init(top: 0, leading: item.priority == .normal ? 8 : 2, bottom: 0, trailing: 8))
-
-                    Spacer()
-                }
-
-                if item.deadline != nil {
-                    HStack {
-                        Image("small-calendar")
-                            .padding(.init(top: -7, leading: 0, bottom: 0, trailing: 0))
-                        Text(item.deadline?.formattedDate ?? "")
-                            .font(Font.system(size: 15))
-                            .foregroundColor(Theme.data.lightTextColor)
-                            .padding(.init(top: -7, leading: 0, bottom: 0, trailing: 0))
-                        Spacer()
-                    }
-                }
-            }
-        }
-    }
-}
-
-struct NewTodoItemCell: View {
-
-    @State
-    var text: String
-
-    var onTextEnter: (String) -> Void
-
-    var body: some View {
-        HStack {
-            TextField(
-                "Новое",
-                text: $text
-            )
-            .onSubmit {
-                self.onTextEnter(text)
-                text = ""
-            }
-            .padding(.init(top: 3, leading: 24, bottom: 3, trailing: 16))
-        }
     }
 }
