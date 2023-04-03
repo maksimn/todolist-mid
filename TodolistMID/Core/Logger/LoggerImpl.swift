09cbedaeda5c1323d2efea3ac28cb44e7f209967
Maksim Ivanov <maksim1989sovunion@yandex.ru>
2023-04-04 02:22:48 +0300

    [TDL-14]: Downgrade iOS version to 13.0.

--- TodolistMID/Core/Logger/LoggerImpl.swift
+++ TodolistMID/Core/Logger/LoggerImpl.swift
@@ -5,26 +5,9 @@
 //  Created by Maxim Ivanov on 07.10.2021.
 //
 
-import os
-
-typealias LoggerApi = os.Logger
-
 final class LoggerImpl: Logger {
 
-    private let isLoggingEnabled: Bool
-    private let loggerApi: LoggerApi
-
-    init(
-        isLoggingEnabled: Bool,
-        category: String
-    ) {
-        self.isLoggingEnabled = isLoggingEnabled
-        loggerApi = LoggerApi(subsystem: "General", category: category)
-    }
-
-    func log(_ message: String, _ level: OSLogType) {
-        guard isLoggingEnabled else { return }
-
-        loggerApi.log(level: level, "\(message)")
+    func log(_ message: String) {
+        print(message)
     }
 }
